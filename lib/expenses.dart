e9935b45b717105f1cb83800427226e2faf5b07e
jowuka <ianilbalci@gmail.com>
2024-07-05 12:54:37 +0200

    todayswork

---
+++ lib/expenses.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  @override
+  State<Expenses> createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(),
+    );
+  }
+}
